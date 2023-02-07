bb3fa9c0c9a0010cf267a46a285bbae173a7ddbf
newfuckingplayer <areej.karayanni@gmail.com>
2023-02-07 17:45:31 +0200

    extended OWZ 1 2,SSZ 1

---
+++ s2.h
@@ -0,0 +1,6 @@
+/* s2.asm-Includefile für C-Programm */
+#define word_728C_user 0x7DA0
+#define Obj5F_MapUnc_7240 0x7E2A
+#define off_3A294 0x3FBA8
+#define MapRUnc_Sonic 0x9F8FC
+/* Ende Includefile für C-Programm */
